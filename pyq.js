378208336e36343473514b3f337e658a773410cb
yashvee2008-max <yashvee2008@gmail.com>
2025-07-17 11:45:52 +0530

    Update pyq.js

--- pyq.js
+++ pyq.js
@@ -3,19 +3,21 @@ async function loadQuestions() {
   const response = await fetch("pyq-data.json");
   const data = await response.json();
   const questions = data[level];
-  
+
   const container = document.getElementById("questions-container");
   container.innerHTML = "";
 
   questions.forEach((q, index) => {
     const qBlock = document.createElement("div");
+    qBlock.className = "question-block";
+
     qBlock.innerHTML = `
       <p><strong>Q${index + 1}:</strong> ${q.question}</p>
-      ${q.options.map(opt => `
-        <label><input type="radio" name="q${index}" value="${opt}"> ${opt}</label><br>
+      ${q.options.map(option => `
+        <label><input type="radio" name="q${index}" value="${option}"> ${option}</label>
       `).join("")}
-      <hr>
     `;
+
     container.appendChild(qBlock);
   });
 }
